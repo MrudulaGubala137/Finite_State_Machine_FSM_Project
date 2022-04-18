6f9dcf153725b01c226c3c0db63ea85a8667c239
PTRL480\Vyshnavi.Gubala <vyshnavi.m@yesgnome.com>
2022-04-18 15:09:40 +0530

    Player material, playerController script, camera follow is added.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int health;
+    bool isDead = false;
+    public GameObject enemy;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
